ba6b0419a1b0fa0ccf89eb05a9247227d2fe82f5
jihye <ji_hye_koo@mckinsey.com>
2023-02-21 11:41:46 -0300

    tryCatch

--- src/main/java/com/example/test/service/ViacepService.java
+++ src/main/java/com/example/test/service/ViacepService.java
@@ -2,18 +2,28 @@ package com.example.test.service;
 
 import com.example.test.client.ViacepFeignClient;
 import com.example.test.model.Address;
+import feign.FeignException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ViacepService {
     private ViacepFeignClient viacepFeignClient;
 
-    public ViacepService(ViacepFeignClient viacepFeignClient){
+    @Autowired
+    public ViacepService(ViacepFeignClient viacepFeignClient) {
+
         this.viacepFeignClient = viacepFeignClient;
     }
 
-    public Address getCep (String cep){
-        Address address = viacepFeignClient.getCep(cep);
-        return  address;
+    public Address getCep(String cep) {
+        try {
+            Address address = viacepFeignClient.getCep(cep);
+            return address;
+        } catch (FeignException feignException){
+            System.out.println("Erro ao pegar cep");
+            return null;
+        }
+
     }
 }
